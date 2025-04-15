2b7c4b41afc849bea48aea428231b4bf31c1a85f
User <user@example.com>
2025-04-15 17:14:10 +0500

    Disable minification in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { minify } from "terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +13,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    minify: false,
   },
   server: {
     port: 3000,
