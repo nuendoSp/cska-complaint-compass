609d2bca57d55b27e03f5be50221be176534cf86
User <user@example.com>
2025-04-16 16:49:04 +0500

    Update ESLint configuration and lint script

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,27 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tseslintParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
+export default [
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -26,4 +33,4 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
     },
   }
-);
+];
